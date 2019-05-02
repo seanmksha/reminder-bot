4f223880d08ba3b55ba819668823e42314a352a2
seanmksha <seanmakosharma@gmail.com>
2019-05-01 21:34:44 -0700

    fixing time formatting again

--- time/time.js
+++ time/time.js
@@ -5,7 +5,7 @@ var self = module.exports={
     main:function(message){
         if(message.content.includes("what")&& message.content.includes("time")&&
         message.content.includes("is")&& message.content.includes("it")){
-            message.channel.send(moment().tz("America/Los_Angeles").format("h:mm  A"+" PDT"));
+            message.channel.send(moment().tz("America/Los_Angeles").format("h:mm  A")+" PDT");
             message.channel.send(moment().tz("America/New_York").format("h:mm  A")+" EST");
            
             
