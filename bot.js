d28a55f36231ea43b72ab10473c16d570478cee3
seanmksha <seanmakosharma@gmail.com>
2019-05-01 17:28:32 -0700

    adding private messages

--- bot.js
+++ bot.js
@@ -2,10 +2,13 @@ const discord = require("discord.js");
 const client = new discord.Client;
 //Create a file key.js that exports your discord token
 const key = require("./key.js");
+//Private messages, in gitignore
+const private = require("./private.js");
+
 
 client.on("message", message => {
     if(message.content == "Hi Bot"){
-        message.reply("Hello World");
+        message.channel.send("Hello World");
     }
     else if(message.content == "Hello Bot" ){
         message.reply("Hello "+message.member.toString()+"!");
