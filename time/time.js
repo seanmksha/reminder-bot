dea855a99ba5b063ac347675c053fa4091e832ea
seanmksha <seanmakosharma@gmail.com>
2019-05-01 21:30:19 -0700

    reformat time

--- time/time.js
+++ time/time.js
@@ -5,8 +5,8 @@ var self = module.exports={
     main:function(message){
         if(message.content.includes("what")&& message.content.includes("time")&&
         message.content.includes("is")&& message.content.includes("it")){
-            message.channel.send(moment().tz("America/Los_Angeles").format("h:mm  A PDT"));
-            message.channel.send(moment().tz("America/New_York").format("h:mm  A EST"));
+            message.channel.send(moment().tz("America/Los_Angeles").format("h:mm  A"+" PDT"));
+            message.channel.send(moment().tz("America/New_York").format("h:mm  A")+" EST");
            
             
         }
