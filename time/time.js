aaba10802e7d601888c37ba8f446eb32bdc20094
seanmksha <seanmakosharma@gmail.com>
2019-05-01 21:26:23 -0700

    adding time zones

--- time/time.js
+++ time/time.js
@@ -1,12 +1,14 @@
 
-const moment = require("moment");
+const moment = require("moment-timezone");
 
 var self = module.exports={
     main:function(message){
         if(message.content.includes("what")&& message.content.includes("time")&&
         message.content.includes("is")&& message.content.includes("it")){
-            message.channel.send(moment().format("h:mm  A"));
-        
+            message.channel.send(moment().tz("America/Los_Angeles").format("h:mm  A PDT"));
+            message.channel.send(moment().tz("America/New_York").format("h:mm  A EST"));
+           
+            
         }
     }
 }
